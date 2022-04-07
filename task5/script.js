98f07f98402d2e3605ea66e4d9dab06c5b1adbdd
Mihail <apanasov.m@yandex.ru>
2022-04-07 12:22:56 +0300

    hw5

---
+++ task5/script.js
@@ -0,0 +1,43 @@
+'use strict'
+
+// task 1
+
+function range (start, end, step = 1){
+    let rangeArray = [];
+    for (let i = start; i <= end; i= i + step){
+        console.log(i);
+        rangeArray.push(i);
+    }
+    return rangeArray;
+}
+console.log(range(3,9,2));
+
+// task 2
+
+function checkSpam (text, ...spamWords){
+    let array = text.split([' ']);
+    console.log(array);
+    console.log(spamWords);
+    let c = 0
+    let mark = 0
+    for (let i = 0, b = spamWords[0]; i < array.length; i++){
+        b = spamWords[0];
+        console.log('обнуляем');
+        for (let num = 0; num < spamWords.length; num++){
+            if (array[i] === spamWords[num]) c++
+        }
+    }
+    if (c === 0){
+        return mark = 'оценка 5'
+    } else if (c > 0 && c < 2){
+        return mark = 'оценка 4'
+    } else if (c > 2 && c < 4){
+        return mark = 'оценка 3'
+    } else if (c > 4 && c < 6){
+        return mark = 'оценка 2'
+    } else if (c > 6 && c < 8){
+        return mark = 'оценка 1'
+    }
+}
+
+console.log(checkSpam('привет привет привет меня зовут бла бла цукк', 'привет', 'бла'))
